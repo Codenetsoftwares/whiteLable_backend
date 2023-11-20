bc2a64b0df726b717ec56bf1db26eff101a61db1
Rikta Dey <145688442+riktadeyy@users.noreply.github.com>
2023-11-20 17:54:24 +0530

    updated

--- models/user.model.js
+++ models/user.model.js
@@ -4,4 +4,5 @@ export const User = new mongoose.model("User", new mongoose.Schema({
     userName:  { type: String, required: true },
     password:   { type: String, required: true },
     roles: { type: String, default: "user" },
+    isActive: {type: Boolean, default: false, required: true}
 }), 'user');
