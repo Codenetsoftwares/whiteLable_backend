7bd325a8bfd190e6ce837acfefc7d519482919ce
Rikta Dey <145688442+riktadeyy@users.noreply.github.com>
2023-11-27 17:58:47 +0530

    locked-status

--- models/admin.model.js
+++ models/admin.model.js
@@ -20,6 +20,7 @@ export const Admin = new mongoose.model("Admin", new mongoose.Schema({
     creditRef : {type : Number, default : 0},
     refProfitLoss : {type : Number, default : 0},
     createBy : {type:ObjectId},
-    isActive: {type: Boolean, default: true, required: true}
+    isActive: {type: Boolean, default: true, required: true},
+    locked : {type : Boolean, default: true, require: true}
       
 }), 'Admin');
