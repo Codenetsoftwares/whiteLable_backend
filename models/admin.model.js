619a54dc13c838e8e85a80194081bafb34b20210
riktadeyy <rikta.dey.cns@gmail.com>
2023-12-01 17:31:15 +0530

    restore data from transh

--- models/admin.model.js
+++ models/admin.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export const Admin = new mongoose.model("Admin", new mongoose.Schema({
     userName:  { type: String, required: true },
-    password:   { type: String, required: true },
+    password:   { type: String,},
     tokens: { ResetPassword: { type: String } },
     roles: [{ type: String, required: true }],
     balance: { type: Number, default: 0},
