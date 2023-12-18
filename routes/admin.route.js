a3c8c138abf30f7094cd1af6c5b4667751e31b0a
riktadeyy <rikta.dey.cns@gmail.com>
2023-12-18 13:18:23 +0530

    Path-create

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -525,4 +525,16 @@ export const AdminRoute = (app) => {
         }
     });
     
+    app.post('/api/Root-Path/:userId/:action', async (req, res) => {
+        const { userId, action } = req.params;
+
+            try {
+                const result = await AdminController.buildRootPath(userId, action);
+                res.status(200).json(result);
+            } catch (error) {
+                res.status(error.code || 500).json({ error: error.message || 'Internal Server Error' });
+            }
+   });
+    
+    
 }
